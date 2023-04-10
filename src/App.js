9e5e4a78a0100fd1a6824b3e5dd3d58052b297b7
Akhlaq Ahmed <akhlaqahmed990@gmail.com>
2023-04-11 03:32:43 +0500

    Routes are added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import About from "./components/About.js"
 import Homepage from "./components/Homepage";
+import MenuPage from "./components/MenuPage.js";
+import Login from "./components/Login.js";
+import BookingPage from "./components/BookingPage.js";
+
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -9,6 +13,12 @@ function App() {
     <Routes>
       <Route path='/' element={<Homepage />} />
       <Route path='/about' element={<About />} />
+      <Route path='/menu' element={<MenuPage />} />
+      <Route path='/bookingpage' element={<BookingPage />} />
+      <Route path='/login' element={<Login />} />
+
+
+
 
     </Routes>
   );
